66e44b736d49a3a649d5c5fbe0816eb95591f314
John Netzel <johnrnetzel@gmail.com>
2022-04-26 21:56:14 -0800

    Add JSON file read functionality to /api/notes route

--- api.js
+++ api.js
@@ -3,16 +3,30 @@ const fs = require("fs");
 
 const api = express.Router();
 
+api.use(express.json());
+
 api.get("/notes", (req, res) =>
 {
     console.log("GET api//notes hit!");
-    res.send("Responding to GET!")
+    fs.readFile(__dirname + "/db/db.json", "UTF-8", (err, file) =>
+    {
+        if(err)
+        {
+            console.error(err);
+            res.json("Error reading notes on server");
+        }
+        else
+        {
+            file = JSON.parse(file);
+            res.json(file);
+        }
+    });
 });
 
 api.post("/notes", (req, res) =>
 {
     console.log("POST api//notes hit!");
-    res.send("Responding to POST!")
+    res.send("Responding to POST!");
 })
 
 module.exports = api;
