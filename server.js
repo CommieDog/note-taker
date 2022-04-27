7c991315b98a50d1d93fc4a3c3c28ed0152c890e
John Netzel <johnrnetzel@gmail.com>
2022-04-26 18:26:55 -0800

    Add HTML routes to server

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) =>
+app.get("/notes", (req, res) =>
 {
-    res.sendFile(path.join(__dirname, '/public/', 'view.html'));
+    res.sendFile(path.join(__dirname, '/public/', 'notes.html'));
+});
+
+app.get("*", (req, res) =>
+{
+    res.sendFile(path.join(__dirname, '/public/', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
